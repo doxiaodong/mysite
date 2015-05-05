f66fbbb267046cf4f87523e80f23e83cf49139e4
duxiaodong <dxdfive@gmail.com>
2015-05-05 15:28:44 +0800

    add handler404

--- mysite/views.py
+++ mysite/views.py
@@ -1,17 +1,7 @@
-from django import http
-from django.template import Context, Engine, TemplateDoesNotExist, loader
+from django.shortcuts import render
 
 
-def page_not_found(request, template_name='404.html'):
+def page_not_found(request):
     context = {'request_path': request.path, 'not_pjax': True}
-    try:
-        template = loader.get_template(template_name)
-        body = template.render(context, request)
-        content_type = None             # Django will use DEFAULT_CONTENT_TYPE
-    except TemplateDoesNotExist:
-        template = Engine().from_string(
-            '<h1>Not Found</h1>'
-            '<p>The requested URL {{ request_path }} was not found on this server.</p>')
-        body = template.render(Context(context))
-        content_type = 'text/html'
-    return http.HttpResponseNotFound(body, content_type=content_type)
+    template = '404.html'
+    return render(request, template, context)
