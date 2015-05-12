8b0e95b8dc9ec03041694d5b5549cef96d36e552
duxiaodong <dxdfive@gmail.com>
2015-05-12 15:39:04 +0800

    recontact socket

--- src/static/js/pages/socket2.js
+++ src/static/js/pages/socket2.js
@@ -9,16 +9,95 @@ window.onload = function () {
         var host = form.getAttribute('host');
         var user = form.getAttribute('user');
         var input = document.getElementById('write_input');
+        var out_socket;
 
         var first_socket = true;
         var protocol = window.location.protocol;
         var ws_protocol = (protocol === 'https:') ? 'wss' : 'ws';
-        var socket = new WebSocket(ws_protocol + host);
+
 
         if (!user) {
             user = prompt('请输入你的昵称！');
         }
 
+
+        var new_socket = function () {
+            var socket = new WebSocket(ws_protocol + host);
+            socket.addEventListener('open', function () {
+                console.log('open');
+            });
+
+            socket.addEventListener('message', function (e) {
+                var data = e.data;
+                try {
+                    data = JSON.parse(data);
+                } catch (e) {
+
+                }
+                switch (data.type) {
+                    case 'add to ul':
+                        var addMessage = function () {
+
+                            var li = document.createElement('li');
+                            li.classList.add('each-message');
+                            var template =
+                                '<span class="left username">' + data.user + ':</span>' +
+                                '<span class="right content">' + data.msg + '</span>';
+                            li.innerHTML = template;
+                            message.appendChild(li);
+                            num_mes++;
+                            if (html.classList.contains('hidden')) {
+                                document.title = '你有' + num_mes + '条新消息喔～～'
+                            }
+                        };
+
+                        var addPerson = function () {
+                            var addRightPerson = function () {
+                                var li = document.createElement('li');
+                                li.classList.add('nowrap');
+                                li.id = data.id;
+                                li.innerHTML = data.user;
+                                user_list.appendChild(li);
+                            };
+                            if (document.getElementById(data.id)) {
+                                return;
+                            } else {
+                                if (data.me === 'me') {
+                                    if (first_socket) {
+                                        addRightPerson();
+                                        document.getElementById(data.id).classList.add('me');
+                                        first_socket = false;
+                                    }
+                                } else {
+                                    addRightPerson();
+                                }
+                            }
+                        };
+
+                        addMessage();
+                        addPerson();
+
+                        break;
+                    case 'remover person':
+                        var remove = document.getElementById(data.id);
+                        if (remove) {
+                            remove.remove();
+                        }
+                }
+            });
+
+            socket.addEventListener('close', function () {
+                console.log('close');
+
+                setTimeout(function () {
+                    out_socket = new_socket();
+                    console.log('reopen');
+                }, 1000);
+            });
+            return socket;
+        };
+
+        out_socket = new_socket();
         form.addEventListener('submit', function (e) {
             if (!user) {
                 user = prompt('请输入你的昵称！');
@@ -31,81 +110,12 @@ window.onload = function () {
                 user: user,
                 msg: input.value
             });
-            socket.send(data);
+            out_socket.send(data);
             input.value = '';
             e.preventDefault();
         });
 
-        socket.addEventListener('open', function () {
-            console.log('open');
-        });
 
-        socket.addEventListener('message', function (e) {
-            var data = e.data;
-            try {
-                data = JSON.parse(data);
-            } catch (e) {
-
-            }
-            switch (data.type) {
-                case 'add to ul':
-                    var addMessage = function () {
-
-                        var li = document.createElement('li');
-                        li.classList.add('each-message');
-                        var template =
-                            '<span class="left username">' + data.user + ':</span>' +
-                            '<span class="right content">' + data.msg + '</span>';
-                        li.innerHTML = template;
-                        message.appendChild(li);
-                        num_mes++;
-                        if (html.classList.contains('hidden')) {
-                            document.title = '你有' + num_mes + '条新消息喔～～'
-                        }
-                    };
-
-                    var addPerson = function () {
-                        var addRightPerson = function() {
-                            var li = document.createElement('li');
-                            li.classList.add('nowrap');
-                            li.id = data.id;
-                            li.innerHTML = data.user;
-                            user_list.appendChild(li);
-                        };
-                        if (document.getElementById(data.id)) {
-                            return;
-                        } else {
-                            if (data.me === 'me') {
-                                if (first_socket) {
-                                    addRightPerson();
-                                    document.getElementById(data.id).classList.add('me');
-                                    first_socket = false;
-                                }
-                            } else {
-                                addRightPerson();
-                            }
-                        }
-                    };
-
-                    addMessage();
-                    addPerson();
-
-                    break;
-                case 'remover person':
-                    var remove = document.getElementById(data.id);
-                    if (remove) {
-                        remove.remove();
-                    }
-            }
-        });
-
-        socket.addEventListener('close', function () {
-            console.log('close');
-
-            setTimeout(function() {
-                socket = new WebSocket(ws_protocol + host);
-            }, 1000);
-        });
     };
 
     socket();
